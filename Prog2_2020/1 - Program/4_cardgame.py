4655ba103753d69757efb78279f6ed417d57f9ec
Karl Enfors <karl.enfors@learnet.se>
2020-09-18 13:07:33 +0200

    u

--- Prog2_2020/1 - Program/4_cardgame.py
+++ Prog2_2020/1 - Program/4_cardgame.py
@@ -2,25 +2,45 @@ import cards
 
 
 DECK = cards.build_deck()
-
-
-print("Prints all cards in list (deck)")
-for CARD in DECK:
-    cards.print_card(CARD)
-
-print("Prints a randomly drawn card")
-cards.print_card(cards.draw_card(DECK,True))
-
-
-print("Prints a card drawn from the top")
-cards.print_card(cards.draw_card(DECK))
-
 cards.shuffle(DECK)
-print("draw 1")
-cards.print_card(cards.draw_card(DECK))
-print("draw 2")
-cards.print_card(cards.draw_card(DECK))
-print("draw 3")
-cards.print_card(cards.draw_card(DECK))
-print("draw 4")
-cards.print_card(cards.draw_card(DECK))
+
+PLAYER1 = cards.draw_card(DECK)
+PLAYER2 = cards.draw_card(DECK)
+
+print(PLAYER1['RANK'])
+print(PLAYER2['RANK'])
+
+for rank in cards.ranks():
+    if PLAYER1['RANK'] == PLAYER2['RANK']:
+        print("Tie!")
+        break
+
+    if rank == PLAYER1['RANK']:
+        print("Player 2 wins...")
+        break
+    if rank == PLAYER2['RANK']:
+        print("Player 1 wins...")
+        break
+
+"""
+points = {
+    'A' : 14,
+    'K' : 13,
+    'D' : 12,
+    'J' : 11,
+    '10': 10,
+    '9': 9,
+    '8': 8,
+    '7': 7,
+    '6': 6,
+    '5': 5,
+    '4': 4,
+    '3': 3,
+    '2': 2,
+}
+
+if (points[PLAYER1['RANK']] > points[PLAYER2['RANK']]):
+    print("Player 1 won...")
+else:
+    print("Player 2 won...")
+"""
