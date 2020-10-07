3a74b4b3fc327f3865d2e9e721e35cd920984fd9
Karl Enfors <karl.enfors@learnet.se>
2020-10-07 13:38:17 +0200

    update

--- Prog2_2020/2 - Arcade/first/KarlGameFile.py
+++ Prog2_2020/2 - Arcade/first/KarlGameFile.py
@@ -2,6 +2,28 @@ import arcade
 
 
 
+
+class KarlBall():
+    pos_x : float
+    pos_y : float
+    dx : float
+    dy : float
+    size : float
+    speed : float
+
+    def __init__(self, x=0, y=0):
+        super().__init__(width,height,title)
+
+        self.pos_x = x
+        self.pos_y = y
+        self.dx = 0.0
+        self.ddx = 0.0
+        self.dy = 0.0
+        self.ddy = 0.0
+        self.speed = 10.0
+        self.size = 50.0
+
+
 class KarlGame(arcade.Window):
 
     WINDOW_W: int
@@ -9,33 +31,79 @@ class KarlGame(arcade.Window):
     TITLE: str
 
 
-    player : object
+
+    pos_x : float
+    pos_y : float
+    dx : float
+    dy : float
+    size : float
+    speed : float
+
 
 
     def __init__(self, width=800, height=660, title="Title"):
         super().__init__(width,height,title)
         self.WINDOW_H = height
         self.WINDOW_W = width
-        #locx = int(1920 / 2)
-        #locy = int(1080 / 2)
-        #self.set_location(locx,locy)
+        self.pos_x = width/2
+        self.pos_y = height/2
+        self.dx = 0.0
+        self.ddx = 0.0
+        self.dy = 0.0
+        self.ddy = 0.0
+        self.speed = 10.0
+        self.size = 50.0
+        
+
 
     def run(self):
         
-        self.player = [0, 0]
 
         arcade.run()
-        arcade.start_render()
-        arcade.set_background_color(arcade.color.RED)
-        arcade.finish_render()
+
+    def on_key_press(self, key, modifiers):
+
+        if key == arcade.key.UP:
+            self.ddy = 1
+        elif key == arcade.key.DOWN:
+            self.ddy = -1
+
+
+        if key == arcade.key.RIGHT:
+            self.ddx = 1
+        elif key == arcade.key.LEFT:
+            self.ddx = -1
+
+
+    def on_key_release(self, key, modifiers):
+
+        if key == arcade.key.UP or key == arcade.key.DOWN:
+            self.ddy = 0
+        if key == arcade.key.RIGHT or key == arcade.key.LEFT:
+            self.ddx = 0
+
+
+    def contain_ball(self):
+        if self.pos_x > self.WINDOW_W or self.pos_x < 0:
+            self.dx *= -3
+        if self.pos_y > self.WINDOW_H or self.pos_y < 0:
+            self.dy *= -3
+
     
     def on_update(self, deltatime):
         """
-        Funktion som körs i början av varje frame, varje "tick". Updaterar alla modeller, och dess värden.
-        """
-        
-
+        Funktion som körs i början av varje frame, varje "tick". 
+        Updaterar alla modeller, och dess värden.
+        """        
         super().on_update(deltatime)
+        self.dx = arcade.lerp(self.dx, self.ddx * self.speed, 0.05)
+        self.dy = arcade.lerp(self.dy, self.ddy * self.speed, 0.05)
+        self.contain_ball()
+
+        self.pos_x += self.dx
+        self.pos_y += self.dy
+
+
     
     def on_draw(self):
         """
@@ -43,7 +111,6 @@ class KarlGame(arcade.Window):
         """
         arcade.start_render()
         arcade.set_background_color(arcade.color.RED)
-        arcade.draw_circle_filled(600.4, 402.2, 50.0,  arcade.color.BLUE)
+        arcade.draw_circle_filled(self.pos_x, self.pos_y, self.size,  arcade.color.BLUE)
 
         super().on_draw()
-        arcade.finish_render()
